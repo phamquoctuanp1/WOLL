c00399244b4766f5f35821b03f9f65de0b1ed5f7
phamquoctuanp1 <71371633+phamquoctuanp1@users.noreply.github.com>
2020-09-21 07:28:38 +0700

    Add files via upload

---
+++ TestWOL/controller.cpp
@@ -0,0 +1,37 @@
+#include "controller.h"
+
+Controller::Controller()
+{
+
+}
+
+void Controller::turn_on(QString macaddress)
+{
+    try
+       {
+           int c{0};
+           unsigned port{60000};
+           bool quiet{false};
+           unsigned long bcast{0xFFFFFFFF};
+
+
+
+           // Parse any remaining arguments (not options).
+           if (optind == 1) {
+               send_wol(macaddress.toStdString(), port, bcast);
+
+               if (!quiet) {
+                   std::cout << "Packet sent to " << std::hex << std::uppercase << htonl(bcast) << '-' << macaddress.toStdString()
+                       << " on port " << std::dec << port << '\n';
+               }
+           } else {
+
+
+           }
+       }
+       catch(const std::exception& e) {
+           std::cerr << e.what() << '\n';
+
+       }
+
+}
